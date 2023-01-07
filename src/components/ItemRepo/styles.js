d402ebc86c3163cc7555e263cf245eae53ca65a0
MarconiJunior <junior@brumafe.com>
2023-01-07 18:30:05 -0300

    Project Finalization

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    margin-left: 1rem;
+    width: 70%;
+    font-size: 20px;
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+    p {
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:20px;
+    }
+    a.remover {
+        color: #FFA000;
+        margin-top:20px;
+        text-decoration: none;
+    }
+    a.view {
+        text-decoration: none;
+    }
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
